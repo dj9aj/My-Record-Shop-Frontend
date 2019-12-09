c220e68e012d2391bdb56dc75838117f57ebee59
Jack Doyle <jackdo@hotmail.co.uk>
2019-12-09 21:42:09 +0000

    Reformat code. Add code comment.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
 
-if (module.hot) {
-  module.hot.accept();
-}
+if (module.hot) module.hot.accept();
